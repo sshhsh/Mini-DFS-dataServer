6fcd9cbadb44d8a36cbd694e1facabf198aab52d
sshhsh <sshhsh@foxmail.com>
2018-05-18 14:40:21 +0800

    add docker kill

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-docker rm dataserver1 dataserver2 dataserver3 datasetver4
+docker kill dataserver1 dataserver2 dataserver3 dataserver4
+docker rm dataserver1 dataserver2 dataserver3 dataserver4
 docker run --name dataserver1 --net dfs -d dataserver
 docker run --name dataserver2 --net dfs -d dataserver
 docker run --name dataserver3 --net dfs -d dataserver
